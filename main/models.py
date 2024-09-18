0ab09b786ad5b5082fc124eb11d910ba612ff578
rahmafira01 <rahmadwimaghfira@gmail.com>
2024-09-18 15:35:29 +0700

    Selesai Tutorial 3

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 import uuid  # tambahkan baris ini di paling atas
@@ -8,6 +9,7 @@ class MoodEntry(models.Model):
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
     mood_intensity = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     @property
     def is_mood_strong(self):
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5@xq49t#_4ws(x2314$gz-b_%wgp3s!+%%36ko#crq45dq3dy!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rahma-dwi31-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
